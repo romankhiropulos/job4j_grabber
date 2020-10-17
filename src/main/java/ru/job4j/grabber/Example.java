23c630a7139db2f1da73381e6d7b47f727d9f72d
romankhiropulos <romankhiropulos@gmail.com>
2020-10-17 16:46:17 +0300

    add checkstyle

---
+++ src/main/java/ru/job4j/grabber/Example.java
@@ -0,0 +1,12 @@
+package ru.job4j.grabber;
+
+public class Example {
+
+    void testMethod() {
+        System.out.println("Hello, Java!");
+    }
+
+    public static void main(String[] args) {
+        new Example().testMethod();
+    }
+}
---
+++ src/test/java/ru/job4j/grabber/ExampleTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.grabber;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void testMethod() {
+
+    }
+}
